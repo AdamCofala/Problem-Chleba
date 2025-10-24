ad114f20412347af046d2e0259951121f6dc8f57
Adam C <9037852u@lo3otwock.eu>
2025-10-24 19:40:53 +0200

    Working DHT sensor - fixed monitor - board data speed

--- ESP32/Esp32 temperature and humidity sensor tests/src/main.cpp
+++ ESP32/Esp32 temperature and humidity sensor tests/src/main.cpp
@@ -1,18 +1,24 @@
-#include <Arduino.h>
+#include "DHT.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define DHTPIN 4
+#define DHTTYPE DHT11  // lub DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.printf("Temp: %.1f°C  Hum: %.1f%%\n", t, h);
+  }
+
+  delay(2500);  // minimum 2 sekundy dla DHT22
 }
